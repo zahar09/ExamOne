222a0a3a38cb5dd7ac4d78f9bd76d9821eaa3b5e
Ilya Zakharov <ilya.zaharov.2009@mail.ru>
2023-08-02 16:27:19 +0300

    Написал программу

---
+++ Program/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите слова через пробел: ");
+string words = Console.ReadLine();
+string[] arr = CreatArrFromString(words);
+PrintAns(array: arr);
+
+string[] CreatArrFromString(string string_arr){
+    string[] arr = string_arr.Split(" ");
+    return arr;
+}
+
+void PrintAns(string[] array){
+    for(int i = 0;i < array.Length;i++){
+        if(array[i].Length <= 3){
+            Console.Write($"{array[i]} ");
+        }
+    }
+}
+
+
